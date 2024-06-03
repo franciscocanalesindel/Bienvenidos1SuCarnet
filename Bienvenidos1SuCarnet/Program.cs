5b50ffc5b3b032bbb8f2ad2c199efda7d251f478
Fran <Fran@FRANCISCO>
2024-06-03 12:25:48 -0600

    Add project files.

---
+++ Bienvenidos1SuCarnet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bienvenidos1SuCarnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la programacion en C#");
+            Console.ReadKey();
+        }
+    }
+}
